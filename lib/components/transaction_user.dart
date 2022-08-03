31c6b6e80ffef1ab1226b40d69c2a6add4d8d60b
Allan Thomas <102838518+Ath0maz@users.noreply.github.com>
2022-08-03 02:37:13 -0300

    chore:'create transactions user page'

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import '../models/transactions.dart';
+import 'package:expensives/components/transaction_form.dart';
+import 'package:expensives/components/transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(_addTransaction),
+      ],
+    );
+  }
+}
