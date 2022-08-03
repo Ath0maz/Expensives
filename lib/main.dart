89f71bbabc728a7b2ff252d850f3255fc8497826
Allan Thomas <102838518+Ath0maz@users.noreply.github.com>
2022-08-03 02:38:13 -0300

    chore:'create function to import new dts'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:expensives/components/transaction_list.dart';
-import 'package:expensives/models/transactions.dart';
+import 'components/transaction_user.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -17,21 +16,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Tenis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +32,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          TransactionList(_transactions),
+          TransactionUser(),
         ],
       ),
     );
